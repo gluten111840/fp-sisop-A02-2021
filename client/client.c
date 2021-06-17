ab564779021f92884b59dbe3a88e6cdc6cefc60a
anandasatriaadi <putu.satriaadi@gmail.com>
2021-06-18 01:00:49 +0800

    Update

--- client/client.c
+++ client/client.c
@@ -4,11 +4,6 @@
 #include <arpa/inet.h>	//inet_addr
 #include <unistd.h>
 #include <ctype.h>
-#include <stdlib.h>
-
-#define SIZE_BUF 100
-
-int handleLog(int server, char uname[SIZE_BUF], char pswrd[SIZE_BUF]);
 
 void recieveInput(const char *title, char str[])
 {
@@ -22,43 +17,48 @@ int main(int argc, char *argv[])
 {
 	int sock;
 	struct sockaddr_in server;
-	char uname[1024], pswrd[1024], unpass[30000];
+	char uname[1024], pswrd[1024];
 	char message[1024], server_reply[1024];
 	//Create socket
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(!getuid())
+	if (sock == -1)
 	{
-		if (sock == -1)
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(7000);
+
+	//Connect to remote server
+	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+	{
+		perror("connect failed. Error");
+		return 1;
+	}
+
+	puts("\e[33mConnected.\e[0m\n");
+
+	printf("Welcome to the A02DB monitor. Commands end with 'enter'.\n");
+	printf("Your A02DB connection is running\n");
+	printf("Server version: 1.0.0-A02DB mendingdb.org binary distribution\n\n");
+	printf("Copyright (c) 2021, A02 Berdua Doang.\n\n");
+	printf("Press ctrl + c to force stop.\n\n");
+
+	/* TO DO
+	TAMBAHIN CODE BUAT KIRIM AUTENTIKASI KE SERVER DISINI
+	BIAR SERVER BISA BEDAIN SUDO ATAU ENGGA
+	*/
+
+	//keep communicating with server
+	int commandTrue = 0;
+	while (1)
+	{
+		// printf("Enter message : ");
+		// scanf("%s", message);
+		if (!getuid())
 		{
-			printf("Could not create socket");
-		}
-		puts("Socket created");
-
-		server.sin_addr.s_addr = inet_addr("127.0.0.1");
-		server.sin_family = AF_INET;
-		server.sin_port = htons(7000);
-
-		//Connect to remote server
-		if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
-		{
-			perror("connect failed. Error");
-			return 1;
-		}
-
-		puts("\e[33mConnected.\e[0m\n");
-
-		printf("Welcome to the A02DB monitor. Commands end with 'enter'.\n");
-		printf("Your A02DB connection is running\n");
-		printf("Server version: 1.0.0-A02DB mendingdb.org binary distribution\n\n");
-		printf("Copyright (c) 2021, A02 Berdua Doang.\n\n");
-		printf("Press ctrl + c to force stop.\n\n");
-
-		int root = 0;
-		//keep communicating with server
-		int commandTrue = 0;
-		while (1)
-		{
-			root = 1;
 			printf("A02DB[root] >> ");
 			fgets(message, sizeof(message), stdin);
 
@@ -80,54 +80,9 @@ int main(int argc, char *argv[])
 			puts(server_reply);
 			memset(&server_reply, '\0', sizeof(server_reply));
 		}
-	}
-	else
-	{
-		if (sock == -1)
+		else
 		{
-			printf("Could not create socket");
-		}
-		puts("Socket created");
-
-		server.sin_addr.s_addr = inet_addr("127.0.0.1");
-		server.sin_family = AF_INET;
-		server.sin_port = htons(7000);
-
-		//Connect to remote server
-		if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
-		{
-			perror("connect failed. Error");
-			return 1;
-		}
-
-		puts("\e[33mConnected.\e[0m\n");
-
-		printf("Welcome to the A02DB monitor. Commands end with 'enter'.\n");
-		printf("Your A02DB connection is running\n");
-		printf("Server version: 1.0.0-A02DB mendingdb.org binary distribution\n\n");
-		printf("Copyright (c) 2021, A02 Berdua Doang.\n\n");
-		printf("Press ctrl + c to force stop.\n\n");
-
-		/* TO DO
-		TAMBAHIN CODE BUAT KIRIM AUTENTIKASI KE SERVER DISINI
-		BIAR SERVER BISA BEDAIN SUDO ATAU ENGGA
-		*/
-
-		strcpy(uname, argv[2]);
-		strcpy(pswrd, argv[4]);
-		sprintf(unpass, "%s:::%s:::\n", uname, pswrd);
-		send(sock, unpass, sizeof(unpass), 0);
-		if (recv(sock, server_reply, 1024, 0) < 0)
-		{
-			printf("Login failed!\n");
-			exit(1);
-		}
-		int root = 0;
-		//keep communicating with server
-		int commandTrue = 0;
-		while (1)
-		{
-			printf("A02DB[%s] >> ", argv[2]);
+			printf("A02DB >> ");
 			fgets(message, sizeof(message), stdin);
 
 			//Send some data
@@ -147,10 +102,31 @@ int main(int argc, char *argv[])
 			puts("Server reply :");
 			puts(server_reply);
 			memset(&server_reply, '\0', sizeof(server_reply));
-			
 		}
+
+		// commandTrue = 0;
+		// while(!commandTrue){
+		// 	char cmd[1024];
+		//     // recieveInput("\nInsert Command >> ", cmd);
+		// 	cmd[strlen(cmd)];
+		// 	printf("\e[0mOurSQL >> \e[36m");
+		// 	scanf("%s", cmd);
+		//     for(int b = 0; b < strlen(cmd); b++){
+		//         cmd[b] = tolower(cmd[b]);
+		//     }
+		//     send(sock, cmd, sizeof(cmd), 0);
+		//     if(!strcmp(cmd, "use")) {
+		//         chdir("");
+		//     }
+		// 	else if(!strcmp(cmd, "")) {
+
+		// 	}
+		// }
+
+		// sleep(2);
+		// if(commandTrue) break;
 	}
+
 	close(sock);
 	return 0;
 }
-
